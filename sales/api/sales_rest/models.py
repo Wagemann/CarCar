aff07e384258d2c95f2f05753fe56983fb3db5c6
Nick <wagemannnick@gmail.com>
2022-08-01 22:29:40 -0400

    Sales Models made

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class AutomobileVO(models.Model):
+    model = models.CharField(max_length=255)
+    color = models.CharField(max_length=255)
+    year = models.PositiveIntegerField()
+    vin = models.PositiveIntegerField(unique=True)
+
+class Employee(models.Model):
+    name = models.CharField(max_length=255)
+    employee_id = models.PositiveIntegerField(unique=True)
+
+class Customer(models.Model):
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    phone_number = models.BigIntegerField(unique=True)
+
+class Record(models.Model):
+    price = models.PositiveIntegerField()
+    employee = models.ForeignKey(
+        Employee,
+        related_name="employee",
+        on_delete=models.CASCADE,
+        null=True)
+    automobile = models.ForeignKey(
+        AutomobileVO,
+        related_name="automobile",
+        on_delete=models.CASCADE,
+        null=True)
+    customer = models.ForeignKey(
+        Customer,
+        related_name="customer",
+        on_delete=models.CASCADE,
+        null=True)
