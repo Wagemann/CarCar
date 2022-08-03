1347937dcb62729c824814c15fadca27d16840b6
Nick <wagemannnick@gmail.com>
2022-08-03 15:50:54 -0400

    progreess frontend

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import ManufacturerList from './ManufacturerList'
 
-function App() {
+function App(props) {
+  if (props.menufacturers === undefined){
+    return null;
+  }
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/inventory/manufacturer/" element={<ManufacturerList manufacturers={props.manufacturers} />} />
+          <Route path="/inventory/manufacturer" element={<ManufacturerList manufacturers={props.manufacturers} />} />
           
         </Routes>
       </div>
