cd3bb1e34de01849d5845e6cdb76a4ccfcde9f99
Nick <wagemannnick@gmail.com>
2022-08-05 11:17:16 -0400

    nav bar dropdowns complete

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -10,29 +10,30 @@ function Nav() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <NavLink className="nav-link" aria-current="page" to="/inventory">Inventory</NavLink>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" role='button' data-bs-toggle='dropdown'aria-expanded="false" >
+              Inventory
+            </a>
+            <ul className="dropdown-menu">
+              <li><a  className="dropdown-item" aria-current="page" href="/inventory/manufacturer/create" >Create Manufacturer</a></li>
+              <li><a  className="dropdown-item" aria-current="page" href="/inventory/manufacturer" >Manufacturer List</a></li>
+              <li><a  className="dropdown-item" aria-current="page" href="/inventory/models/create" >Create Vehicle Model</a></li>
+              <li><a  className="dropdown-item" aria-current="page" href="/inventory/models" >Vehicle Model List</a></li>
+              <li><a  className="dropdown-item" aria-current="page" href="/inventory/automobiles/create" >Create Automobile</a></li>
+              <li><a  className="dropdown-item" aria-current="page" href="/inventory/automobiles" >Automobile List</a></li>
+            </ul>
           </li>
-          <li className="nav-item">
-            <NavLink className="nav-link"  aria-current="page" to="/inventory/manufacturer/create">Create Manufacturer</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" aria-current="page" to="/inventory/manufacturer">Manufacturer List</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" aria-current="page" to="/inventory/models/create">Create Vehicle Model</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" aria-current="page" to="/inventory/models">Vehicle Model List</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" aria-current="page" to="/inventory/automobiles/create">Create Automobile</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" aria-current="page" to="/inventory/automobiles">Automobile List</NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" aria-current="page" to="/sales">Sales</NavLink>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" role='button' data-bs-toggle='dropdown'aria-expanded="false" >
+              Sales
+            </a>
+            <ul className="dropdown-menu">
+              <li><a  className="dropdown-item" aria-current="page" href="/sales/customer/create" >New Customer</a></li>
+              <li><a  className="dropdown-item" aria-current="page" href="/sales/employee/create" >New Employee</a></li>
+              <li><a  className="dropdown-item" aria-current="page" href="/sales/all" >Sales List</a></li>
+              <li><a  className="dropdown-item" aria-current="page" href="/sales/detail" >Employee Sales</a></li>
+              <li><a  className="dropdown-item" aria-current="page" href="/sales/record/create" >Create Sales Record</a></li>
+            </ul>
           </li>
           <li className="nav-item">
             <NavLink className="nav-link" aria-current="page" to="/service">Service</NavLink>
